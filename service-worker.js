8316b9858ee088a4e9cc9ee08c76f8d25f71f8ed
VMSiuP <vmsiup@gmail.com>
2025-05-25 01:07:30 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'escalonador-cache-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
